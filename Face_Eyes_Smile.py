b571373c15629d77e5223f465ca10d308ccdce78
Abhishek Kumar Singh <41370908+abhisngh@users.noreply.github.com>
2018-12-28 03:54:59 +0530

    Updated Cascade Parameter
    Added the comments and manipulated the nearest neighbors for eyes cascades.

--- Face_Eyes_Smile.py
+++ Face_Eyes_Smile.py
@@ -18,7 +18,9 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # We load the cascade for the eyes.
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+# We load the cascade for the smiles.
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+ 
  # We create a function that takes as input the image in black and white (gray) and the original image (frame),
  #and that will return the same image with the detector rectangles. 
 def detect(gray, frame):
@@ -39,15 +41,20 @@ def detect(gray, frame):
         roi_color = frame[y:y+h, x:x+w]
         
         # We apply the detectMultiScale method to locate one or several eyes in the image.
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
         
         
          # For each detected eye:
         for (ex, ey, ew, eh) in eyes:
             # We paint a rectangle around the eyes, but inside the referential of the face.
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+           
+        # We apply the detectMultiScale method to locate smile in the image.
         smile = smile_cascade.detectMultiScale(roi_gray,1.7, 22)
+        
+        # For each detected eye:
         for(sx,sy,sw,sh) in smile:
+            # We paint a rectangle around the smile, but inside the referential of the face.
             cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0,0,2500), 2)
     return frame
 
@@ -58,18 +65,24 @@ video_capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video_capture.read()
+    
     # We do some colour transformations.
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     # We get the output of our detect function.
         canvas = detect(gray, frame)
+    
     # We display the outputs.
         cv2.imshow('Video', canvas)
+    
     # If we type q on the keyboard:
         if cv2.waitKey(1) & 0xFF == ord('q'):
          # We stop the loop.
             break
+
 # We turn the webcam off.    
 video_capture.release()
+
 # We destroy all the windows inside which the images were displayed.
 cv2.destroyAllWindows()
